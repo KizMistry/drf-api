bd60c1dbe28537042f41f6e1a6dcf48147f2bc82
KizMistry <Kiz_mistry@hotmail.com>
2023-05-06 20:02:15 +0000

    update default image

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='image/', default='../default_profile_vq9ill'
+        upload_to='image/', default='../default_profile_haw7w5'
     )
 
     class Meta:
