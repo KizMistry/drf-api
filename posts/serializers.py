7083d30e17321c22f2b974f6d913b78d75d31b67
KizMistry <Kiz_mistry@hotmail.com>
2023-05-16 19:14:40 +0000

    added PostList view, get, post methods & url paths

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from .models import Post
+from posts.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializer.ReadOnlyField(source='owner.profile.id')
-    profile_image =serializer.ReadOnlyField(source='owner.profile.image.url')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -24,12 +24,12 @@ class PostSerializer(serializers.ModelSerializer):
 
     def get_is_owner(self, obj):
         request = self.context['request']
-        return request.owner == obj.owner
+        return request.user == obj.owner
 
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner', 'profile_id', 'Profile_image',
+            'id', 'owner', 'created_at', 'updated_at', 'title',
+            'content', 'image', 'is_owner', 'profile_id', 'profile_image',
             'image_filter',
         ]
