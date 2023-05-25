3894f0f2b596294dd3d218b7afc13d9445a1e923
KizMistry <Kiz_mistry@hotmail.com>
2023-05-25 17:29:46 +0000

    add like, comment count to posts view & serializer

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,5 @@
-# from django.http import Http404
-from rest_framework import status, permissions, generics
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
+from django.db.models import Count
+from rest_framework import permissions, generics, filters
 from .models import Post
 from .serializers import PostSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
@@ -14,7 +12,18 @@ class PostList(generics.ListCreateAPIView):
     """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('likes', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_at',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -26,68 +35,7 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
-
-
-# class PostList(APIView):
-#     serializer_class = PostSerializer
-#     permission_classes = [
-#         permissions.IsAuthenticatedOrReadOnly
-#     ]
-
-#     def get(self, request):
-#         posts = Post.objects.all()
-#         serializer = PostSerializer(
-#             posts, many=True, context={'request': request}
-#         )
-#         return Response(serializer.data)
-
-#     def post(self, request):
-#         serializer = PostSerializer(
-#             data=request.data, context={'request': request}
-#         )
-#         if serializer.is_valid():
-#             serializer.save(owner=request.user)
-#             return Response(
-#                 serializer.data, status=status.HTTP_201_CREATED
-#             )
-#         return Response(
-#             serializer.errors, status=status.HTTP_400_BAD_REQUEST
-#         )
-
-
-# class PostDetail(APIView):
-#     permission_classes = [IsOwnerOrReadOnly]
-#     serializer_class = PostSerializer
-
-#     def get_object(self, pk):
-#         try:
-#             post = Post.objects.get(pk=pk)
-#             self.check_object_permissions(self.request, post)
-#             return post
-#         except Post.DoesNotExist:
-#             raise Http404
-    
-#     def get(self, request, pk):
-#         post = self.get_object(pk)
-#         serializer = PostSerializer(
-#             post, context={'request': request}
-#         )
-#         return Response(serializer.data)
-    
-#     def put(self, request, pk):
-#         post = self.get_object(pk)
-#         serializer = PostSerializer(
-#             post, data=request.data, context={'request': request}
-#         )
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-#     def delete(self, request, pk):
-#         post = self.get_object(pk)
-#         post.delete()
-#         return Response(
-#             status=status.HTTP_204_NO_CONTENT
-#         )
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
